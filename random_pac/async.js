c6cb3eb29efc8399c98db93197e318b7c4aebd0f
0xsadik <torikussadik404@gmail.com>
2024-12-11 01:37:43 +0600

    .

--- random_pac/async.js
+++ random_pac/async.js
@@ -1,1 +1,15 @@
-// 
+// testing asyncronous 
+
+let promise = new Promise((resolve, reject) => {
+    let success = true;
+
+    if (success) {
+        resolve("The operation was success!");
+    }
+    else {
+        reject("Something went wrong!");
+    }
+});
+
+console.log(promise);
+
